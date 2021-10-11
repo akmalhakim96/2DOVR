2033cd1e01079ed9c8a59f7cc933d7238488688a
Masashi Yamada <imashi_16024166@yahoo.co.jp>
2021-10-11 20:10:51 +0900

    HSVの値を変更

--- 2dovr.py
+++ 2dovr.py
@@ -158,7 +158,7 @@ else:
     # 174   97  126 2021/08/06  電気ON,ピンクのテープ貼付
     # 174  124  136 2021/08/13  電気ON,ピンクのテープ貼付
 
-    H = 177; S = 117; V =122 
+    H = 169; S = 99; V =121 
     h_range = 10; s_range = 50; v_range = 50 # 明度の許容範囲
     lower_light = np.array([H-h_range, S-s_range, V-v_range])
     upper_light = np.array([H+h_range, S+s_range, V+v_range])
