cb7270ff0238cf9301713d7811b6cddc303a4679
Masashi Yamada <imashi_16024166@yahoo.co.jp>
2021-10-19 16:35:36 +0900

    imshow=n

--- 2dovr.py
+++ 2dovr.py
@@ -25,7 +25,7 @@ import file_read as fr
 select_hsv = "n" # 画面上で対象物を選択する場合は"y"
 show_res = 'n'   # モータ出力や距離センサの値を表示する場合は "y"
 motor_run = "y"  # モータを回転させる場合は"y"
-imshow = "y"     # カメラが捉えた映像を表示する場合は"y"
+imshow = "n"     # カメラが捉えた映像を表示する場合は"y"
 
 TURN_TIME=0.5
 TURN_POWER=100
