bf6e75f61b5d659832b7f54dcae2290a049b8193
Yamada Masashi <mashi_16024166@yahoo.co.jp>
2021-10-21 10:11:37 +0900

    一斉git pullするプログラムを編集

--- paramiko_2dovr/git.py
+++ paramiko_2dovr/git.py
@@ -9,17 +9,17 @@ user = "pi"
 passwd = "ssr2"
 command2 = 'rm /home/pi/2DOVR/result/*.txt'
 
-print("git resetコマンドを使いますか？(default=y)")
-print("yes: git reset --hard origin/yamada")
-print("no: git -C /home/pi/2DOVR pull")
+print("git pullコマンドを使いますか？(default=y)")
+print("yes: git -C /home/pi/2DOVR pull")
+print("no: git reset --hard origin/yamada")
 
 ch = str(input("y or n ？"))
 if ch == "":
     ch = 'y'
 if ch == 'y':
-    command = 'git reset --hard origin/yamada'
-else:
     command = 'git -C /home/pi/2DOVR pull'
+else:
+    command = 'git reset --hard origin/yamada'
 with open("host.txt") as f:
     s = f.readlines()
 
@@ -45,22 +45,3 @@ for i in range(len(host)):
             print('[stdo]',o,end='')
         for e in stderr:
             print('[err]',e,end='')
-
-print("remove 開始")
-for i in range(len(host)):
-    with paramiko.SSHClient() as ssh:
-        #初回ログイン時の対応
-        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        #ssh接続
-        ssh.connect(host[i],username=user,password=passwd)
-         #コマンド実行
-        stdin,stdout,stderr = ssh.exec_command(command2)
-        print("[" + host[i] + "]")
-        print("[stdo]--->標準出力 , [err]--->エラー出力\n")
-        for o in stdout:
-            print('[stdo]',o,end='')
-        for e in stderr:
-            print('[err]',e,end='')
-
-
-
