9bb983fdfc29cfb35df8cb3d4b42cc985ea19ef2
Masashi Yamada <imashi_16024166@yahoo.co.jp>
2021-10-25 23:36:31 +0900

    a

--- elastic_scattering.py
+++ elastic_scattering.py
@@ -34,7 +34,7 @@ GPIO_R = 18     # 右モーターのgpio 18番
 MAX_SPEED = 62  # パーセント
 DT = 0.05
 dt = DT
-THRESHOLD = 0.2 # OVMをon/offするための閾値
+THRESHOLD = 0.3 # OVMをon/offするための閾値
 
 def motor_out_adjust(vl,vr):
     if vl > 100:
